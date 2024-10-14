9b5f0369d1ebabe8626276bf931076f6536f152b
Kretki <grigoriotvext@mail.ru>
2024-10-15 00:51:00 +0300

    uploadAndDownload

--- static/static3/script.js
+++ static/static3/script.js
@@ -1,9 +1,6 @@
 const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
-const fileInput = document.getElementById("xml-file");
 const loadBtn = document.getElementById("loadBtn");
-const tableSection = document.getElementById("tableSection");
-const tableBody = document.getElementById("tableBody");
 
 // Toggle Sidebar
 menuBtn.addEventListener('click', function() {
@@ -15,3 +12,26 @@ menuBtn.addEventListener('click', function() {
         menuBtn.classList.add("active");
     }
 });
+
+loadBtn.addEventListener('click', function() {
+    fetch('/download')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.blob();
+    })
+    .then(blob => {
+        const file = new Blob([blob], {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+        const url = URL.createObjectURL(file);
+        const link = document.createElement('a');
+        link.download = "enter.xlsx";
+        link.href = url;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    })
+    .catch(error => {
+        console.error('There was a problem with the download:', error);
+    });
+})
