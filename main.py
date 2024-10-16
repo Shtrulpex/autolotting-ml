9300ca191b04471412a8710a9dce8a1c02a13f95
feva <fedorov.va@phystech.edu>
2024-10-16 23:31:42 +0300

    add business-logic of main file

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ from xlsxToCsv import xlxsToDf
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return render_template("home.html")
+
 
 @app.route("/home.html")
 def home():
     return redirect(url_for("main"))
 
+
 @app.route("/orders_page.html")
 def orders():
     return render_template("orders_page.html")
+
 
 @app.route("/lots_page.html")
 def lots():
     return render_template("lots_page.html")
 
+
 FILE_FOLDER = './files'
 if not os.path.exists(FILE_FOLDER):
     os.makedirs(FILE_FOLDER)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -43,6 +49,7 @@ def upload_file():
     else:
         return jsonify({'message': 'Only .xlsx files are allowed'}), 400
 
+
 @app.route('/download', methods=['GET'])
 def download_file():
     csvpath = ""
@@ -50,12 +57,23 @@ def download_file():
         if file.split('.')[-1] == "csv":
             csvpath = "./files/"+file
             break
-    #Здесь добавляете ваш обработчик ML, путь к файлу лежит в csvpath, его же потом сайт и пытается выгрузить пользователю
+
+    # здесь заказ проверяет Inspector
+    # здесь заказ загружается в БД
+    # здесь заказы за месяц выгружаются из БД
+    # здесь заказы за месяц отправляются в autolotting-ml а сформированные лоты попадают в файл по пути csvpath
+    # здесь сформироанные лоты загружаются в БД а сами лоты отправляются пользователю
+    # здесь Scorer считает целевые бизнес-метрики
+    # здесь Analyzer анализирует результаты работы и выдает интересные статистики
+    # здесь Canvas строит графики и настраивает dashboard'ы
+    # здесь на сайт выводятся метрики, параметры, графики и пр.
+
     if os.path.exists(csvpath):
         print(csvpath)
         return send_file(csvpath, as_attachment=True, download_name="lots.csv", mimetype='text/csv')
     else:
         return jsonify({"error": "File not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
