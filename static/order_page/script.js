534ed60e6db01f0e5db9439c4b77351a90f36a9a
Kretki <grigoriotvext@mail.ru>
2024-10-26 19:24:50 +0300

    FirstOrderLoadPage

---
+++ static/order_page/script.js
@@ -0,0 +1,73 @@
+const menuBtn = document.getElementById("menuBtn");
+const sidebarOrders = document.getElementById("sidebar-orders");
+const sidebar = document.getElementById("sidebar");
+const ordersBtn = document.getElementById("ordersBtn");
+const filterInput = document.getElementById("filters");
+const filtersDiv = document.getElementById("filters-box");
+const filtersUl = document.getElementById("filters-ul");
+
+menuBtn.addEventListener('click', function() {
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-300px";
+        menuBtn.classList.remove("active");
+    } else {
+        if (sidebarOrders.style.left === "0px") {
+            sidebarOrders.style.left = "-300px";
+            ordersBtn.classList.remove("active");
+        }
+        sidebar.style.left = "0px";
+        menuBtn.classList.add("active");
+    }
+});
+
+ordersBtn.addEventListener('click', function() {
+    if (sidebarOrders.style.left === "0px") {
+        sidebarOrders.style.left = "-300px";
+        ordersBtn.classList.remove("active");
+    } else {
+        if (sidebar.style.left === "0px") {
+            sidebar.style.left = "-300px";
+            menuBtn.classList.remove("active");
+        }
+        sidebarOrders.style.left = "0px";
+        ordersBtn.classList.add("active");
+    }
+})
+
+filterInput.addEventListener('input', function() {
+    if (filterInput.value.trim() === '') {
+        filterInput.placeholder = 'Добавить фильтры';
+    } else {
+        filterInput.placeholder = '';
+    }
+});
+
+// Alert the input text after typing
+filterInput.addEventListener('blur', function() {
+    if (filterInput.value.trim() !== '') {
+        filterInput.style.color = 'black';
+    }
+    else {
+        filterInput.style.color = 'gray';
+    }
+});
+
+filterInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter'){
+        if (filtersDiv.style.display === '') {
+            filtersDiv.style.display = 'block';
+        }
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+        button.textContent = filterInput.value;
+        button.className = 'order_filter';
+        li.appendChild(button);
+        filtersUl.appendChild(li);
+    }
+})
+
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.className === 'order_filter'){
+        e.target.parentElement.remove();
+    }
+})
