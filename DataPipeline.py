9756d5aa5a34070e16a6ed4d1892acf3cb81fd33
feva <fedorov.va@phystech.edu>
2024-10-26 21:26:17 +0300

    закомментировал лишнее

--- DataPipeline.py
+++ DataPipeline.py
@@ -304,56 +304,58 @@ class DataPipeline:
 
 
 # КОД ДАЛЕЕ НУЖЕН ТОЛЬКО ДЛЯ ФОРМИРОВАНИЯ БД ИЗ CSV ФАЙЛОВ
-id_columns = {
-    'addresses': 'address_id',
-    'classes': 'class_id',
-    'materials': 'material_id',
-    'classes_X_suppliers': 'connection_id',
-    'suppliers': 'supplier_id',
-    'receivers': 'receiver_id',
-    'lottings': 'lotting_id',
-    'requests': 'request_id',
-    'items': 'item_id',
-    'orders': 'order_id',
-    'lots': 'lot_id',
-    'packs': 'pack_id'
-}
+# id_columns = {
+#     'addresses': 'address_id',
+#     'classes': 'class_id',
+#     'materials': 'material_id',
+#     'classes_X_suppliers': 'connection_id',
+#     'suppliers': 'supplier_id',
+#     'receivers': 'receiver_id',
+#     'lottings': 'lotting_id',
+#     'requests': 'request_id',
+#     'items': 'item_id',
+#     'orders': 'order_id',
+#     'lots': 'lot_id',
+#     'packs': 'pack_id'
+# }
+#
+# db_proc = DBProcessor()
+# for table_name in id_columns.keys():
+#     db_proc.run_query(f'DROP TABLE IF EXISTS {table_name};')
+#
+# dp = DataPipeline()
+# dp._db_processor = db_proc
+#
+# import os
+#
+# # Указываем путь к директории с CSV-файлами (9 файлов)
+# csv_folder = os.path.expanduser('~/Desktop/HakatonData')
+# for filename in os.listdir(csv_folder):
+#     if filename.endswith('.csv'):
+#         file_path = os.path.join(csv_folder, filename)
+#         df = pd.read_csv(file_path)
+#         table_name = os.path.splitext(filename)[0]
+#         if table_name == 'template_init_data_for_model_first_month':
+#             # human_lots = df['ID Лота']
+#             # df.drop(columns=['ID Лота'], inplace=True)
+#             # dp.put_requests(df, human_lots=human_lots)
+#             # df = dp.get_requests()
+#             # request_ids = df['№ заявки']
+#             # request_ids = request_ids.astype('Int64')
+#             # df.rename(columns={'№ заявки': 'request_id'}, inplace=True)
+#             # df = pd.DataFrame({
+#             #     'request_id': request_ids,
+#             #     'lot_id': lot_ids
+#             # })
+#             # dp.put_pack('z2020-2', df)
+#             continue
+#         elif table_name == 'z2020-2':
+#             # lot_ids = df['lot_id']
+#             continue
+#         db_proc.load_df(table_name, df, pk_column=id_columns.get(table_name, None))
 
-db_proc = DBProcessor()
-for table_name in id_columns.keys():
-    db_proc.run_query(f'DROP TABLE IF EXISTS {table_name};')
 
-dp = DataPipeline()
-dp._db_processor = db_proc
-
-import os
-
-# Указываем путь к директории с CSV-файлами (9 файлов)
-csv_folder = os.path.expanduser('~/Desktop/HakatonData')
-for filename in os.listdir(csv_folder):
-    if filename.endswith('.csv'):
-        file_path = os.path.join(csv_folder, filename)
-        df = pd.read_csv(file_path)
-        table_name = os.path.splitext(filename)[0]
-        if table_name == 'template_init_data_for_model_first_month':
-            # human_lots = df['ID Лота']
-            # df.drop(columns=['ID Лота'], inplace=True)
-            # dp.put_requests(df, human_lots=human_lots)
-            # df = dp.get_requests()
-            # request_ids = df['№ заявки']
-            # request_ids = request_ids.astype('Int64')
-            # df.rename(columns={'№ заявки': 'request_id'}, inplace=True)
-            # df = pd.DataFrame({
-            #     'request_id': request_ids,
-            #     'lot_id': lot_ids
-            # })
-            # dp.put_pack('z2020-2', df)
-            continue
-        elif table_name == 'z2020-2':
-            # lot_ids = df['lot_id']
-            continue
-        db_proc.load_df(table_name, df, pk_column=id_columns.get(table_name, None))
-
+# КОД ДАЛЕЕ НУЖЕН ТОЛЬКО ДЛЯ ПРОВЕРКИ РАБОТЫ DataPipeline
 # df = dp.get_requests_features()
 # df.to_csv('requests_features.csv', mode='w', index=False)
 # print(df)
