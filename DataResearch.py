63d6375541407f3b9e37e291a97a161289d0db4c
feva <fedorov.va@phystech.edu>
2024-10-29 21:23:10 +0300

    repair validation by standard_shipping parameter

--- DataResearch.py
+++ DataResearch.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+# from datetime import datetime
 
 
 class ValidationError(Exception):
@@ -109,20 +109,24 @@ class Validator:
                     raise TypeMismatchError(col, expected_type, actual_type)
 
         # Проверка нормативных сроков поставки
-        now = pd.Timestamp(datetime.now())
         for index, request in requests.iterrows():
             material_id = request['Материал']
             standard_shipping = self._delivery_standards.loc[self._delivery_standards['material_id'] == material_id,
                                                              'standard_shipping']
             if not standard_shipping.empty:
                 standard_time = standard_shipping.values[0]
-                delivery_time = request['Срок поставки']
-                time_diff = (delivery_time - now).days
+                delivery_time = pd.to_datetime(request['Срок поставки'])
+                # now = pd.Timestamp(datetime.now())
+                # time_diff = (delivery_time - now).days
+                order_time = pd.to_datetime(request['Дата заказа'])
+                time_diff = (delivery_time - order_time).days()
                 if time_diff < standard_time:
                     raise DeliveryTimeError(request['№ заказа'], request['№ позиции'], time_diff, standard_time)
 
         return True
 
+    def validate_lots(self, requests: pd.DataFrame, lots: pd.DataFrame) -> bool:
+        pass
 
 class Scorer:
     def __init__(self):
