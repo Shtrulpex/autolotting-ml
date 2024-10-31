278b828db42f2d9c71e9183d7270acf6cb73a822
feva <fedorov.va@phystech.edu>
2024-10-31 22:31:48 +0300

    efedded

--- DataPipeline.py
+++ DataPipeline.py
@@ -329,7 +329,7 @@ class DataPipeline:
         '''
         return self._db_processor.get_df(query)
 
-    def get_requests_features(self, order_id: Union[int, List[int]], human_lots_essential: bool = True):
+    def get_requests_features(self, order_id: Union[int, List[int]], human_lots_essential: bool = False):
         """
         Выгружает все заявки по зад фильтрам (время + другие) в супер-расширенном формате с указанием всех
          поставщиков заявки
@@ -341,14 +341,14 @@ class DataPipeline:
         else:
             order_condition = f"requests.order_id = {order_id}"
 
-        query_human_lots = f'''
-        SELECT DISTINCT
-            requests.request_id,
-            requests.human_lot_id
-        FROM requests
-        WHERE {order_condition}
-        {'AND human_lot_id IS NOT NULL' if human_lots_essential else ''}
-        '''
+        # query_human_lots = f'''
+        # SELECT DISTINCT
+        #     requests.request_id,
+        #     requests.human_lot_id
+        # FROM requests
+        # WHERE {order_condition}
+        # {'AND human_lot_id IS NOT NULL' if human_lots_essential else ''}
+        # '''
 
         query_request_features = f'''
         WITH t1 AS (
@@ -410,8 +410,11 @@ class DataPipeline:
                 '''
 
         request_features = self._db_processor.get_df(query_request_features)
-        human_lots = self._db_processor.get_df(query_human_lots)
-        return request_features, human_lots
+        request_features['order_dt'] = request_features['order_dt'].astype('datetime64[ns]')
+        request_features['delivery_dt'] = request_features['delivery_dt'].astype('datetime64[ns]')
+        # human_lots = self._db_processor.get_df(query_human_lots)
+        # return request_features, human_lots
+        return request_features
 
     def get_standard_shipping(self):
         """Возвращает стандартые сроки доставки для каждого уникального материала по его классу"""
